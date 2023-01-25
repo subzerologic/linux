cdfa7ca7e6645eafc1d3e728cc7e5e34b415ee11
subzerologic <35272941+subzerologic@users.noreply.github.com>
2023-01-25 16:34:37 -0700

    Update linux_post_install.sh

--- linux_post_install.sh
+++ linux_post_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# run this script: bash linux_post_install.sh 
+# run this script:
+#sudo wget https://github.com/subzerologic/linux/edit/main/linux_post_install.sh -O linux_post_install.sh
+#sudo chmod u+x linux_post_install.sh
+#sudo bash linux_post_install.sh
 
 # apt repository
 sudo apt update
@@ -8,18 +11,21 @@ sudo apt update
 sudo apt install gnome-shell-extension-manager -y
 
 # Install ddclient on Ubuntu 22.04
-sudo apt install ddclient -y
+sudo apt install ddclient
 
 # Install OpenSSH on Ubuntu
 sudo apt install openssh-server -y
 sudo apt install openssh-client -y
 
-# Install wget
-sudo apt install wget -y
+# Change default Port to 9922 and restart ssh
+sudo echo "Port 9922" >> /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+# Allow ssh port on iptables
+sudo iptables -I INPUT -p tcp --dport 9922 -j ACCEPT
 
 # Install OpenVPN
 #Source https://ubuntuhandbook.org/index.php/2022/10/setup-openvpn-ubuntu-2204/amp/
 sudo wget https://git.io/vpn -O openvpn-install.sh
-
 sudo chmod u+x openvpn-install.sh
 sudo bash openvpn-install.sh
